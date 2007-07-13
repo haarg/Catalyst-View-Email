94b4e4ff52960da203b0268451827a6ff74bd5b5
J. Shirley <jshirley@gmail.com>
2007-07-13 17:35:25 +0000

    Version bump to 0.03

--- lib/Catalyst/View/Email.pm
+++ lib/Catalyst/View/Email.pm
@@ -11,7 +11,7 @@ use Email::MIME::Creator;
 
 use base qw|Catalyst::View|;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 __PACKAGE__->mk_accessors(qw(sender stash_key content_type mailer));
 
@@ -198,6 +198,8 @@ J. Shirley <jshirley@gmail.com>
 =head1 CONTRIBUTORS
 
 (Thanks!)
+
+Matt S Trout
 
 Daniel Westermann-Clark
 
